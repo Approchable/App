c4994c26f679779f7004db3f04f5945d35b07007
Timbet Ekanem <Timbet.Ekanem@TimbetEkanem.local>
2022-05-17 07:46:19 +0100

    feature/map_display_hangout_location

--- src/screens/Map/Map.js
+++ src/screens/Map/Map.js
@@ -1,147 +1,333 @@
-import React, {useState, useEffect, useRef} from 'react';
-import { MaterialIcons } from '@expo/vector-icons';
+import React, { useEffect, useLayoutEffect, useRef } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  Animated,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+  Platform,
+} from "react-native";
 import MapView, { Marker, Callout } from 'react-native-maps';
+import { markers, mapDarkStyle, mapStandardStyle } from './mapData';
 import * as Location from 'expo-location';
-import moment from 'moment';
-import { getRegion } from '../../components/config/map';
-import {View,Text,StyleSheet,TextInput,TouchableOpacity,} from 'react-native';
-import {NativeModules} from 'react-native';
-const {StatusBarManager} = NativeModules;
+import LottieView from 'lottie-react-native';
 
-function Map() {
- 
-  const [sendButton,setSendButton]=useState(false)
-  const [messageText,setMessageText]=useState(false)
-   const [messages, setMessage] = useState([
-     {id:1, message:"hi",latitude:"12222", longitude:"123345"},
-     {id:2, message:"hi",latitude:"12222", longitude:"123345"},
-     {id:3, message:"hi",latitude:"12222", longitude:"123345"}
-   ]);
-   const [userlocation,setUserLocation] =useState({})
-   const map =useRef(null)
-   const marker =useRef(null)
+
+
+const { width, height } = Dimensions.get("window");
+const CARD_HEIGHT = 156;
+const CARD_WIDTH = width * 0.8;
+const SPACING_FOR_CARD_INSET = width * 0.1 - 10;
+
+const ExploreScreen = () => {
+    const ASPECT_RATIO = width / height;
+    const initialMapState = {
+        markers,
+        region: {
+          latitude: 51.5051281,
+          longitude: -0.0929633,
+          latitudeDelta: 0.2,
+          longitudeDelta: 0.1,
+        },
+      };
+    
+      const [state, setState] = React.useState(initialMapState);
+      const [location, setLocation] = React.useState(null);
+      let mapIndex = 0;
+  let mapAnimation = new Animated.Value(0);
+
   useEffect(() => {
-  getLocation()
-  },[]);
+    mapAnimation.addListener(({ value }) => {
+      let index = Math.floor(value / CARD_WIDTH + 0.3); // animate 30% away from landing on the next item
+      if (index >= state.markers.length) {
+        index = state.markers.length - 1;
+      }
+      if (index <= 0) {
+        index = 0;
+      }
 
+      clearTimeout(regionTimeout);
 
-  const getLocation = async () => {
- 
-     let { status } = await Location.requestBackgroundPermissionsAsync();
+      const regionTimeout = setTimeout(() => {
+        if( mapIndex !== index ) {
+          mapIndex = index;
+          const { coordinate } = state.markers[index];
+          _map.current.animateToRegion(
+            {
+              ...coordinate,
+              latitudeDelta: state.region.latitudeDelta,
+              longitudeDelta: state.region.longitudeDelta,
+            },
+            350
+          );
+        }
+      }, 10);
+    });
+  });
 
-    if (status === 'granted') {
-      let location = await Location.getCurrentPositionAsync({});
+  const interpolations = state.markers.map((marker, index) => {
+    const inputRange = [
+      (index - 1) * CARD_WIDTH,
+      index * CARD_WIDTH,
+      ((index + 1) * CARD_WIDTH),
+    ];
 
-      setUserLocation({
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude
-       } );
+    const scale = mapAnimation.interpolate({
+      inputRange,
+      outputRange: [1, 1.5, 1],
+      extrapolate: "clamp"
+    });
 
+    return { scale };
+  });
 
+  const onMarkerPress = (mapEventData) => {
+    const markerID = mapEventData._targetInst.return.key;
 
-      //  map.animateToRegion(getRegion(location.coords.latitude, location.coords.longitude, 16000));
+    let x = (markerID * CARD_WIDTH) + (markerID * 20); 
+    if (Platform.OS === 'ios') {
+      x = x - SPACING_FOR_CARD_INSET;
     }
+
+    _scrollView.current.scrollTo({x: x, y: 0, animated: true});
   }
 
+  const _map = React.useRef(null);
+  const _scrollView = React.useRef(null);
 
-  const onChangeText=(messageText) => {
-   if(messageText) {
-    setMessageText(messageText)
-    setSendButton(true)
-   }
-   else{
-    setSendButton(false)
-     console.log("please put a location")
-   }
-   
+  useLayoutEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getLastKnownPositionAsync();
+      setLocation(location);
+      console.log(location.coords.latitude)
+      console.log("gettem")
+    })();
+  }, []);
+
+  const getUserLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permission to access location was denied');
+      return;
+    }
+
+    let location = await Location.getLastKnownPositionAsync();
+    setLocation(location);
+    console.log(location.coords.latitude)
+      console.log("ddddddddd")
   }
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.inputWrapper}>
-        <TextInput
-          style={styles.input}
-          placeholder="Type your message here"
-         onChangeText={messageText => onChangeText(messageText)}
-          value={messageText}
-        />
-        <View style={{ ...styles.sendButton, ...(sendButton ? styles.sendButtonActive : {}) }}>
+  const animation = useRef(null);
+  useEffect(() => {
+    // You can control the ref programmatically, rather than using autoPlay
+    // animation.current?.play();
+  }, []);
+
+
+    return (
         
-          <TouchableOpacity >
-            <MaterialIcons name="search" size={32} color="#030E01" />
-          </TouchableOpacity>
+    <View style={styles.container}>
+       
+       { location === null ? <View style={styles.loader}><Text>Getting Location...</Text></View> : null }
+
+       <MapView ref={_map} style={styles.map} region = {{latitude: location != null ? location.coords.latitude : 26.4475031,longitude: location != null ? location.coords.longitude : -8.1938181,latitudeDelta: location != null ? 0.2 : 0.01,longitudeDelta: location != null ? 0.1 : 0.01}}>
+           {location != null ?
+     <Marker  coordinate={{latitude: location.coords.latitude,longitude: location.coords.longitude}}>
+                   <View style={[styles.markerWrap]}>
+                   <LottieView
+        autoPlay
+        ref={animation}
+        style={{
+          width: 80,
+          height: 80,
+          backgroundColor: 'transparent',
+        }}
+        // Find more Lottie files at https://lottiefiles.com/featured
+        source={require('../../assets/images/assets/position.json')}
+      />
+              </View>
+      </Marker> : null }
+       {state.markers.map((marker, index) => {
+       const scaleStyle = {
+        transform: [
+          {
+            scale: interpolations[index].scale,
+          },
+        ],
+      };
+      return (
+      <Marker  key={index} coordinate={marker.coordinate} onPress={(e)=>onMarkerPress(e)}>
+                   <Animated.View style={[styles.markerWrap]}>
+                  <Animated.View style={{position:"absolute",top:0,right:0,backgroundColor:"#F18D33",borderRadius:"12px",padding:5,zIndex: 3, elevation: 3,marginTop:-10, marginRight:-30}}>
+                  <Text style={{color:"white"}}>in {marker.timing}</Text>
+                  </Animated.View> 
+                <Animated.Image
+                  source={{uri : marker.image}}
+                  style={[styles.marker, scaleStyle]}
+                  resizeMode="cover"
+                />
+              </Animated.View>
+      </Marker> )
+       })}
+      </MapView>
+
+      
+      <TouchableOpacity style={[styles.actionBox, styles.search]}>   
+      <Image style={{width: 20, height: 20}} source={ require('../../assets/images/assets/search.png')} />
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.actionBox, styles.filter]}>
+      <Image style={{width: 20, height: 20}} source={ require('../../assets/images/assets/filter.png')} />
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.actionBox, styles.send]} onPress={getUserLocation}>
+      <Image style={{width: 20, height: 20}} source={ require('../../assets/images/assets/send.png')} />
+      </TouchableOpacity>
+
+
+      <Animated.ScrollView 
+      ref={_scrollView}
+       horizontal
+       pagingEnabled
+       scrollEventThrottle={1}
+       showsHorizontalScrollIndicator={false}
+        style={styles.placesContainer}
+        snapToInterval={CARD_WIDTH + 20}
+        snapToAlignment="center"
+        contentInset={{
+            top: 0,
+            left:0,
+            bottom: 0,
+            right: SPACING_FOR_CARD_INSET
+          }}
+          contentContainerStyle={{
+            paddingHorizontal: Platform.OS === 'android' ? SPACING_FOR_CARD_INSET : 0
+          }}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: {
+                    x: mapAnimation,
+                  }
+                },
+              },
+            ],
+            {useNativeDriver: true}
+          )}
+       >
+            {state.markers.map((marker, index) => { return (
+      <View key={index} style={styles.places}>
+        <View style={{flex:1, flexDirection: "row", justifyContent:"space-between", alignItems: "center"}}>
+            <Text style={{fontSize:16,fontWeight:"600", color:"#030E01"}}>{marker.title}</Text>
+      <View>
+      <Image source={ require('../../assets/images/assets/ArrowRight.png')} />
+      </View>
+        </View>
+
+        <Text style={{fontSize:14, color:"#030E01", marginBottom:5}}>{marker.description}</Text>
+
+        <View style={{flex:1, flexDirection: "row", justifyContent:"space-between", alignItems: "center"}}>
+
+        <View style={{flex:0,flexDirection: "row", alignItems:"center"}}>
+        <Image source={ require('../../assets/images/assets/MapPin.png')} />
+        <Text>Michigan Ave, Chicago</Text>
+        </View>
+
+        <View style={{flex:0,flexDirection: "row", alignItems:"center"}}>
+        <Image source={ require('../../assets/images/assets/Clock.png')} />
+        <Text>8 min walk</Text>
+        </View>
         </View>
       </View>
-      <MapView
-        ref={map}
-        style={styles.map}
-        initialRegion={getRegion(48.860831, 2.341129, 160000)}
-      >
-        {messages.map((message, index) => {
-          let { latitude, longitude, text, timestamp } = message;
-
-          return (
-            <Marker
-              ref={marker}
-              key={index}
-              identifier={'marker_' + index}
-              coordinate={{ latitude, longitude }}
-            >
-              <Callout>
-                <View>
-                  <Text>{text}</Text>
-                  <Text style={{ 'color': '#999' }}>{moment(timestamp).fromNow()}</Text>
-                </View>
-              </Callout>
-            </Marker>
-          )
+           )
         })}
-      </MapView>
+
+      </Animated.ScrollView>
+
     </View>
- 
-  );
 
-
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject
-  },
-  inputWrapper: {
-    width: '100%',
-    position: 'absolute',
-    padding: 10,
-    top: Platform.OS === 'ios' ? 20 : StatusBarManager.HEIGHT,
-    left: 0,
-    zIndex: 100
-  },
-  input: {
-    height: 46,
-    paddingVertical: 10,
-    paddingRight: 50,
-    paddingLeft: 10,
-    borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 6,
-    borderColor: '#ccc',
-    backgroundColor: '#fff'
-  },
-  sendButton: {
-    position: 'absolute',
-    top: 17,
-    right: 20,
-    opacity: 0.4
-  },
-  sendButtonActive: {
-    opacity: 1
-  }
-});
 
-export default Map;
+export default ExploreScreen;
+
+const styles = StyleSheet.create({
+    loader: {
+        position: "absolute",
+    opacity: 0.8,
+    flex: 1,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    width: Dimensions.get('screen').width,
+    height: Dimensions.get('screen').height,
+    elevation: 50,
+    zIndex:50
+    },
+ container : {
+     flex: 1,
+ },
+ actionBox: {
+     right: width * 0.06,
+     position: 'absolute',
+     borderWidth: 1,
+     borderColor: "#ECEEF2",
+     borderRadius: 8,
+  backgroundColor: "white",
+  width: 44,
+  height: 44,
+  justifyContent: "center",
+  alignItems: "center",
+  elevation: 10,
+ },
+ search: {
+    top: height * 0.1, 
+ },
+ filter: {
+    top: height * 0.5, 
+ },
+ send: {
+    top: height * 0.58, 
+ },
+ placesContainer: {
+    top: height * 0.7,
+  position: 'absolute',
+  elevation: 10,
+  paddingHorizontal: 10,
+ },
+ places: {
+    marginHorizontal: 10,
+    height: CARD_HEIGHT,
+    width: CARD_WIDTH,
+    overflow: "hidden",
+    backgroundColor: "white",
+    borderRadius: 12,
+    padding:20
+ },
+ map: {
+    width: Dimensions.get('screen').width,
+    height: Dimensions.get('screen').height,
+  },
+  markerWrap: {
+    position: 'relative',
+    alignItems: "center",
+    justifyContent: "center",
+    width:50,
+    height:50,
+  },
+  marker: {
+    width: 48,
+    height: 48,
+    borderRadius: 48 / 2
+  },
+})
---
+++ src/screens/Map/mapData.js
@@ -0,0 +1,262 @@
+
+
+export const markers = [
+    {
+      coordinate: {
+        latitude: 51.525425010184726,
+        longitude: -0.1066961748843866,
+      },
+      title: "Amazing Food Place",
+      description: "This is the best food place",
+      image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80",
+      rating: 4,
+      reviews: 99,
+      timing: "2h"
+    },
+    {
+      coordinate: {
+        latitude: 51.509188198150234,
+        longitude:-0.06652741449077551
+      },
+      title: "Second Amazing Food Place",
+      description: "This is the second best food place",
+      image: "https://exploringbits.com/wp-content/uploads/2022/01/cool-pfp-5.jpg?ezimgfmt=rs:352x500/rscb3/ng:webp/ngcb3",
+      rating: 5,
+      reviews: 102,
+      timing: "4h"
+    },
+    {
+      coordinate: {
+        latitude: 51.502563625911314,
+        longitude: -0.13244538026490654
+      },
+      title: "Third Amazing Food Place",
+      description: "This is the third best food place",
+      image: "https://miro.medium.com/max/785/0*Ggt-XwliwAO6QURi.jpg",
+      rating: 3,
+      reviews: 220,
+      timing: "Now"
+    },
+    {
+      coordinate: {
+        latitude: 51.47840783709239,
+        longitude: -0.06549744627555469
+      },
+      title: "Fourth Amazing Food Place",
+      description: "This is the fourth best food place",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQthHVYX7uaGAQbJifvkv4GskIjvp14b9KIAQ&usqp=CAU",
+      rating: 4,
+      reviews: 48,
+      timing: "6h"
+    },
+    {
+      coordinate: {
+        latitude: 51.49038044719597,
+        longitude: -0.035285045295744666,
+      },
+      title: "Fifth Amazing Food Place",
+      description: "This is the fifth best food place",
+      image: "https://wallpaperaccess.com/full/2213424.jpg",
+      rating: 4,
+      reviews: 178,
+      timing: "6m"
+    },
+];
+
+export const mapDarkStyle = [
+    {
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#212121"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#212121"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.country",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.locality",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#bdbdbd"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#181818"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#1b1b1b"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#2c2c2c"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#8a8a8a"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#373737"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#3c3c3c"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway.controlled_access",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#4e4e4e"
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#000000"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#3d3d3d"
+        }
+      ]
+    }
+  ];
+
+  export const mapStandardStyle = [
+    {
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+  ];
